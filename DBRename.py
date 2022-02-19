b9df655edb291389ced6736f83a59ad6e0148725
BFTeck <14005701+BFTeck@users.noreply.github.com>
2022-02-19 18:00:24 +0100

    DB rename

--- DBRename.py
+++ DBRename.py
@@ -22,15 +22,17 @@
 #--------------------------------------------#
 import sqlite3
 import os
-import platform
+import re
+import requests
 
 
 
 #--------------------------------------------#
 #                Variables                   #
 #--------------------------------------------#
-a = "Hello World"
-path = "Y:\ATRIER\PatPatrouille"
+
+#path = "Y:\ATRIER\PatPatrouille"
+path = "Y:\Dessins animés - Enfants\Paw_Patrol"
 dbname="57532-paw-patrol.db"
 
 #--------------------------------------------#
@@ -45,19 +47,231 @@ dbname="57532-paw-patrol.db"
 #               Code Principal               #
 #--------------------------------------------#
 
-list = os.listdir(path)
-print(list)
-
-
 conn = sqlite3.connect(dbname)
 c = conn.cursor()
 
+listoffiles = os.listdir(path)
+
+
+for filepath in listoffiles:
+    completepath=path + os.sep + filepath
+    #print(completepath)
+    basename_without_ext = os.path.splitext(os.path.basename(completepath))[0]
+    extension = os.path.splitext(os.path.basename(completepath))[1]
+    #print(basename_without_ext)
+    #print(extension)
+    # filename
+
+    title = basename_without_ext.replace("à", "a")
+    title = title.replace("â", "a")
+    title = title.replace("ù", "u")
+    title = title.replace("û", "u")
+    title = title.replace("ô", "o")
+    title = title.replace("é", "e")
+    title = title.replace("è", "e")
+    title = title.replace("ê", "e")
+    title = title.replace("ç", "c")
+
+    title = title.replace(" !", "")
+    title = title.replace(" ?", "")
+    title = title.replace(" : ", "-")
+    title = title.replace(": ", "-")
+    title = title.replace(":", "-")
+    title = title.replace('"', '')
+    title = title.replace("' ", "_")
+    title = title.replace("'", "_")
+    title = title.replace(" ", "_")
+
+    #print(title)
+    sqlcommand = "SELECT * FROM episode_list WHERE title LIKE '%"+title+"%';"
+    #print(sqlcommand)
+    renamed=False
+    for row in c.execute(sqlcommand):
+        print(row)
+        #La Pat'Patrouille.S06.E24.La Super Patrouille - L'incroyable Hellinger.ts
+        titre_de_lepisode = row[2]
+        duree = titre_de_lepisode.find("___(21_min)")
+        if duree > -1:
+            titre_de_lepisode = titre_de_lepisode[:duree]
+        if int(row[0]) < 10:
+            if int(row[1]) < 10:
+                newcompletepath = path + os.sep + "La Pat'Patrouille.S0" + row[0] + ".E0" + row[
+                    1] + "." + titre_de_lepisode + extension
+            else:
+                newcompletepath = path + os.sep + "La Pat'Patrouille.S0" + row[0] + ".E" + row[
+                    1] + "." + titre_de_lepisode + extension
+        else:
+            if int(row[1]) < 10:
+                newcompletepath = path + os.sep + "La Pat'Patrouille.S" + row[0] + ".E0" + row[
+                    1] + "." + titre_de_lepisode + extension
+            else:
+                newcompletepath = path + os.sep + "La Pat'Patrouille.S" + row[0] + ".E" + row[
+                    1] + "." + titre_de_lepisode + extension
+        print(completepath+" ==> "+newcompletepath)
+        os.rename(completepath, newcompletepath)
+        renamed = True
+    if renamed == False:
+        sqlcommand = "SELECT * FROM episode_list WHERE originaux LIKE '%" + title + "%';"
+        # print(sqlcommand)
+
+        for row in c.execute(sqlcommand):
+            print(row)
+            # La Pat'Patrouille.S06.E24.La Super Patrouille - L'incroyable Hellinger.ts
+            titre_de_lepisode = row[2]
+            duree = titre_de_lepisode.find("___(21_min)")
+            if duree > -1:
+                titre_de_lepisode = titre_de_lepisode[:duree]
+            if int(row[0]) < 10:
+                if int(row[1]) < 10:
+                    newcompletepath = path + os.sep + "La Pat'Patrouille.S0" + row[0] + ".E0" + row[
+                        1] + "." + titre_de_lepisode + extension
+                else:
+                    newcompletepath = path + os.sep + "La Pat'Patrouille.S0" + row[0] + ".E" + row[
+                        1] + "." + titre_de_lepisode + extension
+            else:
+                if int(row[1]) < 10:
+                    newcompletepath = path + os.sep + "La Pat'Patrouille.S" + row[0] + ".E0" + row[
+                        1] + "." + titre_de_lepisode + extension
+                else:
+                    newcompletepath = path + os.sep + "La Pat'Patrouille.S" + row[0] + ".E" + row[
+                        1] + "." + titre_de_lepisode + extension
+            print(completepath + " ==> " + newcompletepath)
+            os.rename(completepath, newcompletepath)
+            renamed = True
+
+listoffiles = os.listdir(path)
+
+for filepath in listoffiles:
+        completepath = path + os.sep + filepath
+        #print(completepath)
+        basename_without_ext = os.path.splitext(os.path.basename(completepath))[0]
+        extension = os.path.splitext(os.path.basename(completepath))[1]
+        # print(basename_without_ext)
+        # print(extension)
+        # filename
+
+        txt = basename_without_ext
+        x = re.search("La Pat'Patrouille.S([0-9]{2}).E([0-9]{2}).(.*)", txt)
+        if x:
+            #print(x)
+            #print(basename_without_ext[26:])
+            fichierimages = basename_without_ext + ".jpg"
+            title = basename_without_ext[26:].replace("à", "a")
+            title = title.replace("â", "a")
+            title = title.replace("ù", "u")
+            title = title.replace("û", "u")
+            title = title.replace("ô", "o")
+            title = title.replace("é", "e")
+            title = title.replace("è", "e")
+            title = title.replace("ê", "e")
+            title = title.replace("ç", "c")
+
+            title = title.replace(" !", "")
+            title = title.replace(" ?", "")
+            title = title.replace(" : ", "-")
+            title = title.replace(": ", "-")
+            title = title.replace(":", "-")
+            title = title.replace('"', '')
+            title = title.replace("' ", "_")
+            title = title.replace("'", "_")
+            title = title.replace(" ", "_")
+            sqlcommand = "SELECT * FROM episode_list WHERE title = '" + title + "';"
+            # print(sqlcommand)
+            correct = False
+            for row in c.execute(sqlcommand):
+
+                if int(row[0]) < 10:
+                    checksaison = "0" + row[0]
+                else:
+                    checksaison = row[0]
+
+                if int(row[1]) < 10:
+                    checkepisode = "0" + row[1]
+                else:
+                    checkepisode = row[1]
+
+
+                if checksaison == x[1]:
+                    if checkepisode == x[2]:
+                        correct = True
+
+            if correct == False:
+                #print("Pblm avec "+ completepath)
+                #print("Saison: " +checksaison+ " vs " + x[1])
+                #print("Episode: " + checkepisode + " vs " + x[2])
+
+                sqlcommand = "SELECT * FROM episode_list WHERE title LIKE '%" + title + "%';"
+                for row in c.execute(sqlcommand):
+                    titre_de_lepisode = row[2]
+                    duree = titre_de_lepisode.find("___(21_min)")
+                    if duree > -1:
+                        titre_de_lepisode=titre_de_lepisode[:duree]
+                    if int(row[0]) < 10:
+                        if int(row[1]) < 10:
+                            newcompletepath = path + os.sep + "La Pat'Patrouille.S0" + row[0] + ".E0" + row[1] + "." + titre_de_lepisode + extension
+                        else:
+                            newcompletepath = path + os.sep + "La Pat'Patrouille.S0" + row[0] + ".E" + row[1] + "." + titre_de_lepisode + extension
+                    else:
+                        if int(row[1]) < 10:
+                            newcompletepath = path + os.sep + "La Pat'Patrouille.S" + row[0] + ".E0" + row[1] + "." + titre_de_lepisode + extension
+                        else:
+                            newcompletepath = path + os.sep + "La Pat'Patrouille.S" + row[0] + ".E" + row[1] + "." + titre_de_lepisode + extension
+
+                    print(completepath + " ==> " + newcompletepath)
+                    os.rename(completepath, newcompletepath)
+
+for filepath in listoffiles:
+        completepath = path + os.sep + filepath
+        #print(completepath)
+        basename_without_ext = os.path.splitext(os.path.basename(completepath))[0]
+        extension = os.path.splitext(os.path.basename(completepath))[1]
+        # print(basename_without_ext)
+        # print(extension)
+        # filename
+
+        txt = basename_without_ext
+        x = re.search("La Pat'Patrouille.S([0-9]{2}).E([0-9]{2}).(.*)", txt)
+        if x:
+            #print(x)
+            #print(basename_without_ext[26:])
+            fichierimages = basename_without_ext + ".jpg"
+            completepath = path + os.sep + fichierimages
+            imagedone = False
+            if os.path.isfile(completepath):
+                #print("Image find for: "+ basename_without_ext)
+                imagedone = True
+            else:
+                title = basename_without_ext[26:].replace("à", "a")
+                title = title.replace("â", "a")
+                title = title.replace("ù", "u")
+                title = title.replace("û", "u")
+                title = title.replace("ô", "o")
+                title = title.replace("é", "e")
+                title = title.replace("è", "e")
+                title = title.replace("ê", "e")
+                title = title.replace("ç", "c")
+
+                title = title.replace(" !", "")
+                title = title.replace(" ?", "")
+                title = title.replace(" : ", "-")
+                title = title.replace(": ", "-")
+                title = title.replace(":", "-")
+                title = title.replace('"', '')
+                title = title.replace("' ", "_")
+                title = title.replace("'", "_")
+                title = title.replace(" ", "_")
+                sqlcommand = "SELECT * FROM episode_list WHERE title = '" + title + "';"
+                # print(sqlcommand)
+                for row in c.execute(sqlcommand):
+                    print("Fichier a telecharger: " + row[4])
+                    img_data = requests.get(row[4]).content
+                    with open(completepath, 'wb') as handler:
+                        handler.write(img_data)
+                    imagedone = True
+
+
+
 # verif table
-sqlcommand = "select * from episode_list;"
-print(sqlcommand)
-c.execute(sqlcommand)
 
-
-conn.commit()
 
 conn.close()
--- TMDBExtract.py
+++ TMDBExtract.py
@@ -53,14 +53,14 @@ path = url_decopose.path.split('/')
 seriename = path[2]
 
 
-
+os.remove(seriename+'.db')
 
 conn = sqlite3.connect(seriename+'.db')
 c = conn.cursor()
 
 # Create table
 c.execute(
-    '''CREATE TABLE IF NOT EXISTS `episode_list`(saisons varchar(250)  NOT NULL default '',episodes varchar(250)  NOT NULL default '',title varchar(250)  NOT NULL default '' );''')
+    '''CREATE TABLE IF NOT EXISTS `episode_list`(saisons varchar(250)  NOT NULL default '',episodes varchar(250)  NOT NULL default '',title varchar(250)  NOT NULL default '',originaux varchar(250)  NOT NULL default '',images varchar(250)  NOT NULL default '' );''')
 #scraper = cfscrape.create_scraper()  # returns a CloudflareScraper instance
 # One TLS v1.3 cipher and one TLS v1.2 cipher
 cloudscraper.DEFAULT_CIPHERS = 'TLS_AES_256_GCM_SHA384:ECDHE-ECDSA-AES256-SHA384'
@@ -72,24 +72,67 @@ scraper = cloudscraper.create_scraper()
 index = 0
 endoflist = False
 precedent=""
+original=""
+image=""
 while endoflist == False:
     newurl = url + '/season/' + str(index)+'?language=fr-FR'
     print(newurl)
     html_doc = scraper.get(newurl).content
     soup = BeautifulSoup(html_doc, 'html.parser')
-    liste_ep = soup.find_all("a", class_="no_click open")
+    liste_ep = soup.find_all("div", class_="card")
 
     for x in liste_ep:
-        #print(x)
-        idx_episode = str(x).find('episode="')+9
-        idx_title = str(x).find('title="')+7
-        idx_title = str(x).find('-',idx_title) + 2
+
+        idx_episode = str(x).find('<a class="no_click open" episode="')+34
+        #idx_title = str(x).find('title="')+7
+        #idx_title = str(x).find('-',idx_title) + 2
+        idx_title = str(x).find('<img alt="')+10
+        idx_img = str(x).find('srcset="') + 8
         idx_fepisode = str(x).find('"', idx_episode)
+        #idx_ftitle = str(x).find('"', idx_title)
         idx_ftitle = str(x).find('"', idx_title)
+        idx_fimg = str(x).find('"', idx_img)
+
         #print("EP Debut: " + str(idx_episode)+" - Fin: " + str(idx_fepisode))
         #print("TIT Debut: " + str(idx_title) + " - Fin: " + str(idx_ftitle))
+        #print("IMG Debut: " + str(idx_img) + " - Fin: " + str(idx_fimg))
+        #print(x)
+
+        if (idx_img >8):
+            setdimages = str(x)[idx_img:idx_fimg]
+            #print(setdimages)
+            aimages = setdimages.split(",")
+            #print(aimages)
+            idx_lostimage = len(aimages) - 1
+            #print(aimages[idx_lostimage])
+            #print(aimages[idx_lostimage].split(" "))
+            aimages = aimages[idx_lostimage].strip().split(" ")
+            image = website + aimages[0]
+            #print("Image: " + image)
+        else:
+            image=""
+            idx_title = str(x).find('title="')+7
+            idx_title = str(x).find('-',idx_title) + 2
+            idx_ftitle = str(x).find('"', idx_title)
+
         episode=str(x)[idx_episode:idx_fepisode]
+        #print("Episode: "+ episode)
+
         title=str(x)[idx_title:idx_ftitle]
+        #print("Titre: "+ title)
+
+        title = title.replace("à", "a")
+        title = title.replace("â", "a")
+        title = title.replace("ù", "u")
+        title = title.replace("û", "u")
+        title = title.replace("ô", "o")
+        title = title.replace("é", "e")
+        title = title.replace("è", "e")
+        title = title.replace("ê", "e")
+        title = title.replace("ç", "c")
+
+        title = title.replace(" !", "")
+        title = title.replace(" ?", "")
         title = title.replace(" : ", "-")
         title = title.replace(": ", "-")
         title = title.replace(":", "-")
@@ -98,13 +141,40 @@ while endoflist == False:
         title = title.replace("'","_")
         title = title.replace(" ","_")
 
+        newepurl = url + "/season/"+str(index)+"/episode/"+episode+"?language=en-EN"
+        #print(newepurl)
+        html_doc = scraper.get(newepurl).content
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        idx_titleen= str(soup).find('<h3><a class="no_click open" episode="'+episode+'"')+35
+        idx_titleen = str(soup).find('>',idx_titleen)+1
+        idx_ftitleen = str(soup).find('<', idx_titleen)
+        original=str(soup)[idx_titleen:idx_ftitleen]
+        original = original.replace("à", "a")
+        original = original.replace("â", "a")
+        original = original.replace("ù", "u")
+        original = original.replace("û", "u")
+        original = original.replace("ô", "o")
+        original = original.replace("é", "e")
+        original = original.replace("è", "e")
+        original = original.replace("ê", "e")
+        original = original.replace("ç", "c")
+
+        original = original.replace(" !", "")
+        original = original.replace(" ?", "")
+        original = original.replace(" : ", "-")
+        original = original.replace(": ", "-")
+        original = original.replace(":", "-")
+        original = original.replace('"', '')
+        original = original.replace("' ", "_")
+        original = original.replace("'", "_")
+        original = original.replace(" ", "_")
 
         trouve="S" + str(index)+".E"+episode+"."+title
 
 
         if trouve != precedent:
-            print("S" + str(index)+".E"+episode+"."+title)
-            sqlcommand = "INSERT INTO episode_list (saisons,episodes , title) VALUES ('"+str(index)+"','"+episode+"','"+title+"');"
+            #print("S" + str(index)+".E"+episode+"."+title)
+            sqlcommand = "INSERT INTO episode_list (saisons,episodes , title, originaux, images) VALUES ('"+str(index)+"','"+episode+"','"+title+"','"+original+"','"+image+"');"
             print(sqlcommand)
             c.execute(sqlcommand)
         precedent=trouve
